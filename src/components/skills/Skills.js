cbaac363f0527996699a5f9c039176411eae9337
Jacob Namhyung Kim <87889917+everywherejacobkim@users.noreply.github.com>
2022-04-23 01:41:21 -0700

    feat: add skills page & styling

--- src/components/skills/Skills.js
+++ src/components/skills/Skills.js
@@ -1,4 +1,5 @@
-import React from 'react'
+import React from 'react';
+import {MdGppGood} from 'react-icons/md';
 import './skills.css';
 
 const Skills = () => {
@@ -6,6 +7,106 @@ const Skills = () => {
     <section id="skills">
       <h5>What Slills I have</h5>
       <h2>My Experience</h2>
+
+      <div className="container skills__container">
+        <div className="skills__left"> 
+          <h3 className="skills__left__title">Web Development</h3>
+          <div className="skills__left__box">
+
+            <article className="skills__left__detail">
+              <MdGppGood className="skills__icon"/>
+              <div>
+                <h3 className="skills__title">HTML/CSS</h3>
+                <h5 className="skills__level">Experienced</h5>
+              </div>
+            </article>
+
+            <article className="skills__left__detail">
+              <MdGppGood className="skills__icon"/>
+              <div>
+                <h3 className="skills__title">Javascript</h3>
+                <h5 className="skills__level">Experienced</h5>
+              </div>
+            </article>
+            <article className="skills__left__detail">
+              <MdGppGood className="skills__icon"/>
+              <div>
+                <h3 className="skills__title">React JS</h3>
+                <h5 className="skills__level">Experienced +</h5>
+              </div>  
+            </article>
+            <article className="skills__left__detail">
+              <MdGppGood className="skills__icon"/>
+              <div>
+                <h3 className="skills__title">Tailwind CSS</h3>
+                <h5 className="skills__level">Experienced</h5>
+              </div>
+            </article>
+            <article className="skills__left__detail">
+              <MdGppGood className="skills__icon"/>
+              <div>
+                <h3 className="skills__title">Node JS</h3>
+                <h5 className="skills__level">Intermediate</h5>
+              </div>
+            </article>
+            <article className="skills__left__detail">
+              <MdGppGood className="skills__icon"/>
+              <div>
+                <h3 className="skills__title">MongoDB</h3>
+                <h5 className="skills__level">Experienced</h5>
+              </div>  
+            </article>
+          </div>
+        </div>
+        
+        <div className="skills__right"> 
+          <h3 className="skills__right__title">Mobile App Development</h3>
+          <div className="skills__right__box">
+            <article className="skills__right__detail">
+              <MdGppGood className="skills__icon"/>
+              <div>
+                <h3 className="skills__title">React Native</h3>
+                <h5 className="skills__level">Experienced +</h5>
+              </div>
+            </article>
+            <article className="skills__right__detail">
+              <MdGppGood className="skills__icon"/>
+              <div>
+                <h3 className="skills__title">Java</h3>
+                <h5 className="skills__level">Intermediate</h5>
+              </div>
+            </article>
+            <article className="skills__right__detail">
+              <MdGppGood className="skills__icon"/>
+              <div>
+                <h3 className="skills__title">Kotlin</h3>
+                <h5 className="skills__level">Basic</h5>
+              </div>
+            </article>
+            <article className="skills__right__detail">
+              <MdGppGood className="skills__icon"/>
+              <div>
+                <h3 className="skills__title">Objective C</h3>
+                <h5 className="skills__level">Basic</h5>
+              </div>
+            </article>
+            <article className="skills__right__detail">
+              <MdGppGood className="skills__icon"/>
+              <div>
+                <h3 className="skills__title">Swift</h3>
+                <h5 className="skills__level">Basic</h5>
+              </div>
+            </article>
+            <article className="skills__right__detail">
+              <MdGppGood className="skills__icon"/>
+              <div>
+                <h3 className="skills__title">Ionic</h3>
+                <h5 className="skills__level">Basic</h5>
+              </div>
+            </article>
+          </div>
+        </div>  
+      </div>
     </section>
   )
 }
