854c5d44c7666123c0072aafc3c4dd2d1a8c60c0
Jacob Namhyung Kim <87889917+everywherejacobkim@users.noreply.github.com>
2022-12-30 00:22:08 -0800

    Chore: update resume download

--- src/components/header/Resume.js
+++ src/components/header/Resume.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import texture from '../../img/bg/texture.png';
+import resume from '../../img/resume/Jacob_Namhyung_Kim_Resume.pdf';
 import {FaFileDownload} from 'react-icons/fa';
 import { motion } from "framer-motion";
 import './header.css';
@@ -7,7 +7,7 @@ import './header.css';
 const Resume = () => {
     return (
         <div className="resumeBtns">
-            <motion.a href={texture} download className='btn' animate={{color: ['#fff', '#ceefff']}} transition={{ delay: 2, duration: 3}}>
+            <motion.a href={resume} download className='btn' animate={{color: ['#fff', '#ceefff']}} transition={{ delay: 2, duration: 3}}>
             <FaFileDownload />&nbsp; Resume</motion.a>
             <a href="#contact" className='btn btn-primary'>Contact Me</a>
         </div>
