3236ee7b8df912353281d7bda5b2be1f3628062b
Jacob Namhyung Kim <87889917+everywherejacobkim@users.noreply.github.com>
2023-03-27 13:39:10 -0700

    Chore: add myDailyNote project

--- src/components/projects/Projects.js
+++ src/components/projects/Projects.js
@@ -2,31 +2,32 @@ import React from 'react';
 import { Card, CardActionArea, CardContent, CardMedia, Typography } from '@mui/material';
 import Pomodee from '../../img/project/pomodee.png';
 import HandyChef from '../../img/project/handyChef.png';
+import MyDailyNote from '../../img/project/myDailyNote.png';
 import './projects.css';
 
 const Projects = ({ projectRef, projectIsVisible }) => {
 
   const projects = [
     {
+      title: "Handy Chef",
+      image: HandyChef,
+      stack: "React Native | Node.js | Express.js | MongoDB",
+      desc: "HandyChef is a cross-platform mobile app which connects independent chefs with clients to bring a private restaurant experience to the clients’ homes at an affordable rate.",
+      url: "https://www.handychef.ca"
+    },
+    {
       title: "Pomodee",
       image: Pomodee,
-      stack: "React.js | Node.js | Express.js | MongoDB",
+      stack: "React | Node.js | Express.js | MongoDB",
       desc: "Pomodee is a fun-and-network-driven timer app based on pomodoro technique which connects with others to collaborate in real-time study & work sessions.",
       url: "https://pomodee.com"
     },
     {
-      title: "Handy Chef",
-      image: HandyChef,
-      stack: "React Native | Node.js | Express.js | MongoDB",
-      desc: "HandyChef is a cross-platform mobile app which connects independent chefs with clients to bring a private restaurant experience to the clients’ homes at an affordable rate.",
-      url: "https://www.handychef.ca"
-    },
-    {
-      title: "Handy Chef",
-      image: HandyChef,
-      stack: "React Native | Node.js | Express.js | MongoDB",
-      desc: "HandyChef is a cross-platform mobile app which connects independent chefs with clients to bring a private restaurant experience to the clients’ homes at an affordable rate.",
-      url: "https://www.handychef.ca"
+      title: "My Daily Note",
+      image: MyDailyNote,
+      stack: "React | PWA",
+      desc: "My Daily Note offers a daily to-do section, memo recording, and motivational quotes, with a user-friendly interface to help users track their activities, capture notes, and stay motivated.",
+      url: "https://eloquent-begonia-351848.netlify.app"
     },
   ]
 
